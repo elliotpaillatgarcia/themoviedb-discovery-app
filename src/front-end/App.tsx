8c793754e9612e7505ef8aabaf9e8e0751ae40e2
elliotpaillatgarcia <elliot.paillat.garcia@gmail.com>
2026-09-23 15:07:46 +0000

    chore: 🔧 big commit

--- src/front-end/App.tsx
+++ src/front-end/App.tsx
@@ -1,9 +1,11 @@
-import { useEffect, useState } from "react"
-import type { Movie } from "../back-end/schemas/MoviesTypes"
+import { useEffect, useState } from 'react';
+import type { Movie } from '../back-end/schemas/MoviesTypes';
+import MovieItem from './components/MovieItem';
+import './app.css';
 
 export default function App() {
   // State to hold the fetched movies data, initialized to null
-  const [movies, setMovies] = useState<Movie[] | null>(null)
+  const [movies, setMovies] = useState<Movie[] | null>(null);
 
   // useEffect hook to fetch data from an API when the component mounts
   useEffect(() => {
@@ -11,25 +13,38 @@ export default function App() {
     fetch('/api/movies/popular')
       .then((response) => response.json())
       .then((data) => {
-        console.log('Fetched movies data:', data) // Log the fetched data for debugging
-        setMovies(data.results) // Update the state with the fetched movies data
+        console.log('Fetched movies data:', data); // Log the fetched data for debugging
+        setMovies(data.results); // Update the state with the fetched movies data
       })
-  }, [])
+      .catch((error) => {
+        console.error('Error fetching popular movies:', error);
+      });
+  }, []);
+
   return (
-      <div>
-        <h1>Popular Movies</h1>
+    <main className="app-shell">
+      <header className="app-header">
+        <h1>Films populaires</h1>
+        <h2>
+          Films tendances en France, d'après les données de{' '}
+          <b>The Movie Database</b>
+        </h2>
+      </header>
+      <section>
         {movies ? (
-          <ul>
+          <ul className="movie-grid">
             {movies.map((movie) => (
               <li key={movie.id}>
-                <h2>{movie.title}</h2>
-                <p>{movie.overview}</p>
-                <p>Release Date: {movie.release_date}</p>
-                <p>Rating: {movie.vote_average}</p>
+                <article>
+                  <MovieItem movie={movie} />
+                </article>
               </li>
             ))}
           </ul>
-        ) : null}
-      </div>
-    )
+        ) : (
+          <p className="status-message">Loading...</p>
+        )}
+      </section>
+    </main>
+  );
 }
